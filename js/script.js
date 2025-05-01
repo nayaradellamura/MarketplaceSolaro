8a118d1e5e0c1d744a2d7b1a334ee299142811e2
Nayara Dellamura <168658191+nayaradellamura@users.noreply.github.com>
2025-05-01 13:37:48 -0300

    ajustes no modal de login e cadastro

--- js/script.js
+++ js/script.js
@@ -1,10 +1,43 @@
-function openModal() {
-    var myModal = new bootstrap.Modal(document.getElementById('modal'));
-    myModal.show();
+function openFormularioModal() {
+  var loginModal = bootstrap.Modal.getInstance(document.getElementById('loginModal'));
+  if (loginModal) {
+      loginModal.hide();
   }
-  
-  function closeModal() {
-    var myModal = bootstrap.Modal.getInstance(document.getElementById('modal'));
-    myModal.hide();
+
+  var myModal = new bootstrap.Modal(document.getElementById('modal'));
+  myModal.show();
+}
+
+function closeFormularioModal() {
+  var myModal = bootstrap.Modal.getInstance(document.getElementById('modal'));
+  myModal.hide();
+}
+
+function openLoginModal() {
+  var formularioModal = bootstrap.Modal.getInstance(document.getElementById('modal'));
+  if (formularioModal) {
+      formularioModal.hide();
   }
-  
+
+  var modal = new bootstrap.Modal(document.getElementById('loginModal'));
+  modal.show();
+}
+
+function closeLoginModal() {
+  var modalEl = document.getElementById('loginModal');
+  var modal = bootstrap.Modal.getInstance(modalEl);
+  modal.hide();
+}
+
+function validarLogin() {
+  const email = document.getElementById('usuario').value;
+  const senha = document.getElementById('senha').value;
+
+  if (!email || !senha) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+  }
+
+  alert('Login enviado!');
+}
+
