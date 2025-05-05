fc77c56a5047e060b9ebb2e6c6f246270ee14987
Matheus Opuscolo <matheus.opuscolo@gmail.com>
2025-05-05 20:04:51 -0300

    Crude com os Forms

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ dotenv.config({ pat: './.env' })
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -30,6 +33,38 @@ db.connect( (error) => {
 app.get("/", (req, res) =>{
     res.render("index")
 });
+
+app.get('/index', (req, res) => {
+    res.render('index'); 
+  });
+  
+  app.post('/cadastro', (req, res) => {
+    const {
+      cpf_cnpj, nome, contato, tipo,
+      cep, rua, numero, bairro, cidade, estado, pais,
+      cadastroEmail, cadastroSenha
+    } = req.body;
+  
+    const sql = `
+      INSERT INTO usuarios (
+        cpf_cnpj, nome, contato, tipo,
+        cep, rua, numero, bairro, cidade, estado, pais,
+        email, senha
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+  
+    db.query(sql, [
+      cpf_cnpj, nome, contato, tipo,
+      cep, rua, numero, bairro, cidade, estado, pais,
+      cadastroEmail, cadastroSenha
+    ], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.send('Erro ao cadastrar usuário.');
+      }
+      res.send('Usuário cadastrado com sucesso!');
+    });
+  });
 
 app.listen(5000, () => {
     console.log("Servidor Rodanddo na Porta 5000");
