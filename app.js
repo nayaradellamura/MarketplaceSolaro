74f78c33d42a15da210a34f2e32c94366c1cebde
Nayara Dellamura <168658191+nayaradellamura@users.noreply.github.com>
2025-05-05 22:17:29 -0300

    ajuste na rota do partials login

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const path = require('path');
-const mysql = require("mysql"); 
-const dotenv = require('dotenv');
-const e = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const hbs = require("hbs");
 
-dotenv.config({ pat: './.env' })
+dotenv.config({ path: './.env' });
 
 const app = express();
 
+// Middleware para ler dados de formulário e JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Conexão com o banco de dados
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -18,37 +20,47 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
 
-const DiretorioPublico = path.join(__dirname, './public');
-app.use(express.static(DiretorioPublico))
+db.connect((err) => {
+    if (err) {
+        console.error("Erro ao conectar no MySQL:", err);
+    } else {
+        console.log("MySQL conectado...");
+    }
+});
+
+// Configurar caminhos
+const publicDir = path.join(__dirname, './public');
+app.use(express.static(publicDir));
 
 app.set('view engine', 'hbs');
-   
-db.connect( (error) => {
-    if(error){
-        console.log(erro)
-    } else{
-        console.log("MySQL Conectado...")
-    }
-})
 
-// Definir Rotas
+// Registrar diretório de partials
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+// Rotas principais
 app.use('/', require('./routes/pages'));
 
+// Rotas extras
 app.get('/index', (req, res) => {
-    res.render('index'); 
-  });
+    res.render('index');
+});
 
-  app.get('/home', (req, res) => {
-    res.render('const_index'); 
-  });
-  
-  app.post('/cadastro', (req, res) => {
+app.get('/home', (req, res) => {
+    res.render('const_index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Rota para cadastro de usuários
+app.post('/cadastro', (req, res) => {
     const {
-      cpf_cnpj, nome, contato, tipo,
-      cep, rua, numero, bairro, cidade, estado, pais,
-      cadastroEmail, cadastroSenha
+        cpf_cnpj, nome, contato, tipo,
+        cep, rua, numero, bairro, cidade, estado, pais,
+        cadastroEmail, cadastroSenha
     } = req.body;
-  
+
     const sql = `
       INSERT INTO usuarios (
         cpf_cnpj, nome, contato, tipo,
@@ -56,20 +68,21 @@ app.get('/index', (req, res) => {
         email, senha
       ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
-  
-    db.query(sql, [
-      cpf_cnpj, nome, contato, tipo,
-      cep, rua, numero, bairro, cidade, estado, pais,
-      cadastroEmail, cadastroSenha
-    ], (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.send('Erro ao cadastrar usuário.');
-      }
-      res.send('Usuário cadastrado com sucesso!');
-    });
-  });
 
+    db.query(sql, [
+        cpf_cnpj, nome, contato, tipo,
+        cep, rua, numero, bairro, cidade, estado, pais,
+        cadastroEmail, cadastroSenha
+    ], (err, result) => {
+        if (err) {
+            console.error("Erro ao cadastrar usuário:", err);
+            return res.send('Erro ao cadastrar usuário.');
+        }
+        res.send('Usuário cadastrado com sucesso!');
+    });
+});
+
+// Iniciar servidor
 app.listen(5000, () => {
-    console.log("Servidor Rodanddo na Porta 5000");
-})
+    console.log("Servidor rodando na porta 5000");
+});
