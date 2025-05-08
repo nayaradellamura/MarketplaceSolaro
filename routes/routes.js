41bb21a8f502d409d964fb755db7858ce9672528
Nayara Dellamura <168658191+nayaradellamura@users.noreply.github.com>
2025-05-08 20:49:56 -0300

    ajustes nas rotas

--- routes/routes.js
+++ routes/routes.js
@@ -7,8 +7,14 @@ const usuarioController = require('../controllers/usuarioController');
 router.get('/', (req, res) => res.render('index'));
 router.get('/index', (req, res) => res.render('index'));
 router.get('/home', (req, res) => res.render('home'));
+
+// Partials
 router.get('/login', (req, res) => res.render('login'));
 router.get('/cadastro', (req, res) => res.render('form_cadastro'));
+router.get('/head', (req, res) => res.render('head'));
+router.get('/footer', (req, res) => res.render('footer'));
+router.get('/navbar_index', (req, res) => res.render('navbar_index'));
+router.get('/navbar_home', (req, res) => res.render('navbar_home'));
 router.get('/cadastro_usuario', (req, res) => res.render('cadastro_usuario'));
 
 // Processamento de formulários
