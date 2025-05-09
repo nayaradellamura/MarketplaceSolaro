0520a7d0e57ec8896a4b2692e6143f74bdf72861
Matheus Opuscolo <matheus.opuscolo@gmail.com>
2025-05-09 20:46:47 -0300

    Hash da senha

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,31 +1,38 @@
-
 const db = require('../db/db');
+const bcrypt = require('bcrypt');
 
-exports.cadastrarUsuario = (req, res) => {
+exports.cadastrarUsuario = async (req, res) => {
     const {
         cpf_cnpj, nome, contato, tipo,
         cep, rua, numero, bairro, cidade, estado, pais,
         cadastroEmail, cadastroSenha
     } = req.body;
 
-    const sql = `
-        INSERT INTO usuarios (
+    try {
+        const hashedSenha = await bcrypt.hash(cadastroSenha, 10); 
+
+        const sql = `
+            INSERT INTO usuarios (
+                cpf_cnpj, nome, contato, tipo,
+                cep, rua, numero, bairro, cidade, estado, pais,
+                email, senha
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+
+        db.query(sql, [
             cpf_cnpj, nome, contato, tipo,
             cep, rua, numero, bairro, cidade, estado, pais,
-            email, senha
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+            cadastroEmail, hashedSenha
+        ], (err, result) => {
+            if (err) {
+                console.error("Erro ao cadastrar usuário:", err);
+                return res.send('Erro ao cadastrar usuário.');
+            }
+            res.redirect('/index?showLoginModal=true');
+        });
 
-    db.query(sql, [
-        cpf_cnpj, nome, contato, tipo,
-        cep, rua, numero, bairro, cidade, estado, pais,
-        cadastroEmail, cadastroSenha
-    ], (err, result) => {
-        if (err) {
-            console.error("Erro ao cadastrar usuário:", err);
-            return res.send('Erro ao cadastrar usuário.');
-        }
-        res.redirect('/index?showLoginModal=true');
-
-    });
+    } catch (error) {
+        console.error("Erro ao encriptar a senha:", error);
+        res.send('Erro ao processar cadastro.');
+    }
 };
